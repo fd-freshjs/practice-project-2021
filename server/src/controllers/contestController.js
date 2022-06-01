6a4e7659540e3f8a931a20b45e29efd592dcbf73
Yevhenii Lapshynov <fd-freshjs>
2022-06-01 17:50:05 +0300

    Fix get rating mark

--- server/src/controllers/contestController.js
+++ server/src/controllers/contestController.js
@@ -108,7 +108,9 @@ module.exports.getContestById = async (req, res, next) => {
     await Promise.all(
       contestInfo.Offers.map(async (v, i) => {
         const user = await dbClient.Users.findByPk(v.userId);
+        const rating = await dbClient.Ratings.findOne({ where: { userId: req.tokenData.userId } });
         contestInfo.Offers[i].User = user;
+        contestInfo.Offers[i].mark = rating.mark;
       }),
     );
     res.send(contestInfo);
